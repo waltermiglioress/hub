a356caa0ac4672e7b48de793288a51ed101696c2
waltermiglioress <walter.migliore@sicilsaldo.it>
2024-03-25 19:43:43 +0100

    number_format for widget

--- app/Filament/Resources/ProductionResource/Widgets/ProductionOverview.php
+++ app/Filament/Resources/ProductionResource/Widgets/ProductionOverview.php
@@ -23,19 +23,19 @@ class ProductionOverview extends BaseWidget
     {
         return [
 //            Stat::make('Produzione fatturata', '€'.$this->getPageTableQuery()->where('imponibile','>',0,'and',)->sum('imponibile'))
-            Stat::make('Produzione fatturata', '€'.Production::query()->where('imponibile','>',0,'and')->where('status','fatturato')->sum('imponibile'))
+            Stat::make('Produzione fatturata', '€'.number_format(Production::query()->where('imponibile','>',0,'and')->where('status','fatturato')->sum('imponibile'),2,',','.'))
 //                ->description('32k increase')
                 ->descriptionIcon('heroicon-m-arrow-trending-up')
                 ->color('success'),
-            Stat::make('Produzione stimata', '€'.Production::query()->where('imponibile','>',0,'and')->where('status','stimato')->sum('imponibile'))
+            Stat::make('Produzione stimata', '€'.number_format(Production::query()->where('imponibile','>',0,'and')->where('status','stimato')->sum('imponibile'),2,',','.'))
 //                ->description('7% increase')
                 ->descriptionIcon('heroicon-m-arrow-trending-down')
                 ->color('estimated'),
-            Stat::make('Produzione contabilizzata e non fatturata', '€'.Production::query()->where('imponibile','>',0,'and')->where('status','contabilizzato e non ft')->sum('imponibile'))
+            Stat::make('Produzione contabilizzata e non fatturata','€'.number_format( Production::query()->where('imponibile','>',0,'and')->where('status','contabilizzato e non ft')->sum('imponibile'),2,',','.'))
 //                ->description('3% increase')
                 ->descriptionIcon('heroicon-m-arrow-trending-up')
                 ->color('danger'),
-            Stat::make('Produzione in corso', '€'.Production::query()->where('imponibile','>',0,'and')->where('status','in corso')->sum('imponibile'))
+            Stat::make('Produzione in corso', '€'.number_format(Production::query()->where('imponibile','>',0,'and')->where('status','in corso')->sum('imponibile'),2,',','.'))
 //                ->description('3% increase')
                 ->descriptionIcon('heroicon-m-arrow-trending-up')
                 ->color('primary'),
